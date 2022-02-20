4c28f5ac5124dfef1368c0a4b33ee7b11f68a7d9
landofcoder <landofcoder@gmail.com>
2022-02-20 17:14:11 +0700

    completed resolver

--- Model/Resolver/SubmitForm.php
+++ Model/Resolver/SubmitForm.php
@@ -16,6 +16,9 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\GraphQl\Model\Query\ContextInterface;
 use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Lof\Formbuilder\Api\SubmitFormRepositoryInterface;
+use Lof\Formbuilder\Api\Data\SubmitFormFieldInterfaceFactory;
+use Lof\Formbuilder\Api\Data\SubmitFormInterfaceFactory;
+use Lof\Formbuilder\Api\Data\SubmitFormInterface;
 
 class SubmitForm implements ResolverInterface
 {
@@ -26,15 +29,31 @@ class SubmitForm implements ResolverInterface
     private $submitFormRepository;
 
     /**
+     * @var SubmitFormFieldInterfaceFactory
+     */
+    private $submitFormFieldFactory;
+
+    /**
+     * @var SubmitFormInterfaceFactory
+     */
+    private $submitFormFactory;
+
+    /**
      * construct
      *
      * @param SubmitFormRepositoryInterface $submitFormRepository
+     * @param SubmitFormFieldInterfaceFactory $submitFormFieldFactory
+     * @param SubmitFormInterfaceFactory $submitFormFactory
      */
     public function __construct(
-        SubmitFormRepositoryInterface $submitFormRepository
+        SubmitFormRepositoryInterface $submitFormRepository,
+        SubmitFormFieldInterfaceFactory $submitFormFieldFactory,
+        SubmitFormInterfaceFactory $submitFormFactory
     )
     {
         $this->submitFormRepository = $submitFormRepository;
+        $this->submitFormFieldFactory = $submitFormFieldFactory;
+        $this->submitFormFactory = $submitFormFactory;
     }
 
     /**
@@ -51,13 +70,50 @@ class SubmitForm implements ResolverInterface
         if (false === $context->getExtensionAttributes()->getIsCustomer()) {
             throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
         }
-        if (empty($args['input'])) {
+        if (empty($args['input'] || empty($args["input"]["form_id"])) || empty($args["input"]["fields"])) {
             throw new GraphQlInputException(__('"input" value should be specified'));
+        }
+
+        $submitFormData = $this->formatSubmitFormData($args['input']);
+
+        if (!$submitFormData->getFields()) {
+            throw new GraphQlInputException(__('"fields" value should be specified'));
         }
 
         $customerId = $context->getUserId();
         $store = $context->getExtensionAttributes()->getStore();
 
-        return $this->submitFormRepository->submitForm((int)$customerId, $args['input'], $store->getId());
+        return $this->submitFormRepository->submitForm((int)$customerId, $submitFormData, $store->getId());
+    }
+
+    /**
+     * format submit form data
+     *
+     * @param mixed|array $input
+     * @return SubmitFormInterface
+     */
+    protected function formatSubmitFormData($input = []): SubmitFormInterface
+    {
+        $submitFormData = $this->submitFormFactory->create();
+        $submitFormData->setFormId((int)$input["form_id"]);
+        $submitFormData->setProductId(isset($input["product_id"]) ? (int)$input["product_id"] : 0);
+        $submitFormData->setCaptcha(isset($input["captcha"]) ? $input["captcha"] : "");
+
+        $fields = [];
+        foreach ($input["fields"] as $_item) {
+            if (!isset($_item["cid"]) || empty($_item["cid"])) {
+                continue;
+            }
+            $formFieldData = $this->submitFormFieldFactory->create();
+            $formFieldData->setCid($_item["cid"]);
+            $formFieldData->setFieldName(isset($_item["field_name"]) ? $_item["field_name"] : "");
+            $formFieldData->setValue(isset($_item["value"]) ? $_item["value"] : "");
+
+            $fields[] = $formFieldData;
+        }
+
+        $submitFormData->setFields($fields);
+
+        return $submitFormData;
     }
 }
